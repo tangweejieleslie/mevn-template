7415273e7a0fe47f1345e66ff657078729dc032a
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-05-27 17:42:56 +0800

    SSL test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
+
+app.use(bodyParser.json({limit: '3mb'}));
 app.use(cors());
 
 const api = require('./api');
@@ -14,5 +20,16 @@ app.get('/',(req,res)=>{
 	res.send('This is an ExpressJS App');
 });
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// const port = process.env.PORT || 4040;
+// app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// console.log(__dirname);
+
+const httpOptions = {
+	cert: fs.readFileSync(path.join(__dirname, 'ssl', 'server.crt')),
+	key: fs.readFileSync(path.join(__dirname, 'ssl', 'server.key'))
+}
+
+https.createServer(httpOptions, app).listen(port, ()=>{
+	console.log('Listening on port', port);
+});
